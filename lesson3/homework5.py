fb91cb0413b3f89cfd54dabb053bc1ad31bbc224
AlexB <AlexB@mail.com>
2021-11-14 23:20:37 +0300

    homework5 + homework6

--- lesson3/homework5.py
+++ lesson3/homework5.py
@@ -6,3 +6,42 @@
 # Если специальный символ введен после нескольких чисел, то вначале нужно добавить сумму этих чисел к полученной ранее
 # сумме и после этого завершить программу.
 
+def special_symbol(input_string):
+    """
+    Return True if special symbol in input_string and False if it isn't
+    :param input_string: str
+    :return: bool
+    """
+    result = False
+    for ch in ["!", "@", "#", "$", "%", "^", "&", "*"]:
+        # print(f"{ch}  {input_string}")
+        if ch in input_string:
+            result = True
+            break
+    return result
+
+def my_func(input_string):
+    """
+    Return sum of numbers in input_string
+    :param input_string: str
+    :return: int
+    """
+    my_list = input_string.split(' ')
+    result = 0
+    for num in my_list:
+        if special_symbol(num):
+            break
+        result += int(num)
+    return result
+
+result = 0
+
+while True:
+    input_str = input("""Special symbols: "!", "@", "#", "$", "%", "^", "&", "*"
+Enter your string (for exit enter special symbol): """)
+    result += my_func(input_str)
+    if special_symbol(input_str):
+        break
+    print(result)
+
+print(result)
